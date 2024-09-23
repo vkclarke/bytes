81684c312672a0cb8f9471d1233723444153a2b4
Vincent <shotgun@FW25FZ1.g.stan>
2024-09-22 21:44:04 -0400

    let's use a "proper" iterator now

--- bytes.go
+++ bytes.go
@@ -3,23 +3,28 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"iter"
 	"os"
 )
 
-func main() {
-	pipe := make(chan byte, os.Getpagesize())
-
-	go func() {
-		in := bufio.NewReader(os.Stdin)
-
-		for b, eof := in.ReadByte(); eof == nil; b, eof = in.ReadByte() {
-			pipe <- b
+func bytes(in io.ByteReader) iter.Seq[byte] {
+	return func(yield func(byte) bool) {
+		for {
+			if b, err := in.ReadByte(); err != nil {
+				if err == io.EOF {
+					break
+				}
+				panic(err)
+			} else if !yield(b) {
+				break
+			}
 		}
+	}
+}
 
-		close(pipe)
-	}()
-
-	for b := range pipe {
+func main() {
+	for b := range bytes(bufio.NewReader(os.Stdin)) {
 		fmt.Println(b)
 	}
 }
